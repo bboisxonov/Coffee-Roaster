9dc3c96f722b170fc1997f67ca1845f8ad3e5174
Behruz <bboisxonov@yandex.com>
2023-05-08 18:14:01 +0500

    plan section js WIP

--- js/main.js
+++ js/main.js
@@ -1,2 +1,54 @@
+// "use strict"
+// const elPlanBlock = document.querySelector('.coffee_plan');
+// const elsPlanToggler = document.querySelectorAll('.coffee_plan--toggler');
 
+
+// // if (elsPlanToggler) {
+// //     elsPlanToggler.addEventListener('click', function () {
+// //         elPlanBlock.classList.toggle('coffee_plan--open')
+// //     })
+// // }
+
+// elsPlanToggler.forEach(function () {
+//     elsPlanToggler.addEventListener('click', function () {
+//         elPlanBlock.classList.toggle('coffee_plan--open')
+//     });
+// })
+
+
+
+
+const elPlanBlock = document.querySelector('.coffee_plan');
+const elPlanToggler1 = document.querySelector('.plan-toggler1');
+const elPlanToggler2 = document.querySelector('.plan-toggler2');
+const elPlanToggler3 = document.querySelector('.plan-toggler3');
+const elPlanToggler4 = document.querySelector('.plan-toggler4');
+const elPlanToggler5 = document.querySelector('.plan-toggler5');
+
+
+if (elPlanToggler1) {
+    elPlanToggler1.addEventListener('click', function () {
+        elPlanBlock.classList.toggle('coffee_plan--open1')
+    })
 }
+if (elPlanToggler2) {
+    elPlanToggler2.addEventListener('click', function () {
+        elPlanBlock.classList.toggle('coffee_plan--open2')
+    })
+}
+if (elPlanToggler3) {
+    elPlanToggler3.addEventListener('click', function () {
+        elPlanBlock.classList.toggle('coffee_plan--open3')
+    })
+}
+if (elPlanToggler4) {
+    elPlanToggler4.addEventListener('click', function () {
+        elPlanBlock.classList.toggle('coffee_plan--open4')
+    })
+}
+if (elPlanToggler5) {
+    elPlanToggler5.addEventListener('click', function () {
+        elPlanBlock.classList.toggle('coffee_plan--open5')
+    })
+}
+
